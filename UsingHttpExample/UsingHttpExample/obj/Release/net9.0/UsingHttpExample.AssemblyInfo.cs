f011d39fe8355ac75e29afc4a1535c0b4aee0a82
Nadi <nadinaeremina@mail.ru>
2025-05-19 11:40:30 +0500

    http

--- UsingHttpExample/UsingHttpExample/obj/Release/net9.0/UsingHttpExample.AssemblyInfo.cs
+++ UsingHttpExample/UsingHttpExample/obj/Release/net9.0/UsingHttpExample.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsingHttpExample")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf7b2b0d3233736ccc2936c60f5e92c5a4efd2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsingHttpExample")]
